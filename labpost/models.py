3fcb8f2fcd31801b4e67e80d4d24ced94d82994c
Bishal Karki <bishaldobato@gmail.com>
2018-07-29 19:29:24 +0545

    lab record test

--- labpost/models.py
+++ labpost/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # lab test record model
-FLAG_CHOICE = ('N', 'H', 'L',)
+FLAG_CHOICE = [('N', 'Normal'), ('H', 'High'), ('L','Low'),]
 class TestItem (models.Model):
     """This class is the actual record item
        to get the lab test."""
 
     id = models.AutoField(primary_key = True)
     testName = models.CharField(max_length=20)
-    result = models.FloatField()
-    flag = models.CharField(max_length = 1)
-    reference = models.TextField(max_length = 100)
+    result = models.FloatField(default=0)
+    flag = models.CharField(max_length = 1, choices = FLAG_CHOICE)
+    reference = models.CharField(max_length = 20)
 
     def __str__(self):
         return self.testName
--- labpost/views.py
+++ labpost/views.py
@@ -1,24 +1,49 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.forms import formset_factory
 
 from .forms import TestItemForm
-from django.forms import formset_factory
 
 
 # Create your views here.
 def labReport(request):
     """Generate lab test report form"""
 
+    testItemFormset = formset_factory(TestItemForm, extra=2)
+
+    # if request.method == 'POST':
+    #     testItem = TestItemForm(request.POST)
+
+    #     if testItem.is_valid():
+    #         testItem.save()
+
+    #     return HttpResponseRedirect("")
+
+    # if request.method == 'POST':
+    #     formset = testItemFormset(request.POST)
+    #     if formset.is_valid():
+    #         # formset = formset.cleaned_data
+    #         for test in formset:
+    #             if test.has_changed():
+    #                 test.save()
+
+    #     return HttpResponseRedirect("")
+
     if request.method == 'POST':
-        testItem = TestItemForm(request.POST)
+        formset = testItemFormset(request.POST)
+        if formset.is_valid():
+            for test in formset:
+                data = test.cleaned_data
+                flag = True
+                for _, value in data:
+                    if value == '':
+                        flag = False
+                        break
+                if flag:
+                    test.save()
 
-        if testItem.is_valid():
-            # testItem = testItem.cleaned_data
-            testItem.save()
-
-            return HttpResponseRedirect("")
-
+        return HttpResponseRedirect("")
 
     else:
-        return render(request,'template.html',{'form': TestItemForm})
+        return render(request, 'template.html', {'formset': testItemFormset,})
